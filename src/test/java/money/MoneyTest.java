a004bb2cecac1094f8b47fe5a2ca270af14f316a
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-03-08 15:52:21 +0900

    Chapter 3 is done.

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -12,4 +12,10 @@ public class MoneyTest {
         product = five.times(3);
         assertEquals(15, product.amount);
     }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
+    }
 }
