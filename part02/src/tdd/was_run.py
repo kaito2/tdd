689e577bc1c6dd5c4fb38ee6b9678083e0942b5b
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-03-11 16:48:26 +0900

    Chapter 19 is done.

--- part02/src/tdd/was_run.py
+++ part02/src/tdd/was_run.py
@@ -1,11 +1,20 @@
-class WasRun:
-    def __init__(self, name):
-        self.wasRun = None
+class TestCase:
+    def __init__(self, name: str):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+
+class WasRun(TestCase):
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
--- part02/tests/xunit.py
+++ part02/tests/xunit.py
@@ -1,6 +1,18 @@
-from src.tdd.was_run import WasRun
+from src.tdd.was_run import WasRun, TestCase
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testRunning(self):
+        self.test.run()
+        assert (self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert (self.test.wasSetUp)
+
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
