ed128d8d376e168249fae3832be49bb6f7d7b759
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-03-11 20:13:02 +0900

    Apply mypy.

--- part02/src/tdd/was_run.py
+++ part02/src/tdd/was_run.py
@@ -1,61 +1,64 @@
-class TestCase:
-    def __init__(self, name: str):
-        self.name = name
+from typing import Callable, List
 
-    def set_up(self):
+
+class TestResult:
+    def __init__(self) -> None:
+        self.run_count: int = 0
+        self.error_count: int = 0
+
+    def test_started(self) -> None:
+        self.run_count += 1
+
+    def test_failed(self) -> None:
+        self.error_count += 1
+
+    def summary(self) -> str:
+        return f"{self.run_count} run, {self.error_count} failed"
+
+
+class TestCase:
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+
+    def set_up(self) -> None:
         pass
 
-    def run(self, result):
+    def run(self, result: TestResult) -> None:
         result.test_started()
         self.set_up()
         try:
-            method = getattr(self, self.name)
+            method: Callable = getattr(self, self.name)
             method()
         except:
             result.test_failed()
         self.tear_down()
 
-    def tear_down(self):
+    def tear_down(self) -> None:
         pass
 
 
-class TestResult:
-    def __init__(self):
-        self.run_count = 0
-        self.error_count = 0
-
-    def test_started(self):
-        self.run_count += 1
-
-    def test_failed(self):
-        self.error_count += 1
-
-    def summary(self):
-        return f"{self.run_count} run, {self.error_count} failed"
-
-
 class TestSuite:
-    def __init__(self):
-        self.tests = []
+    def __init__(self) -> None:
+        self.tests: List[TestCase] = []
 
-    def add(self, test):
+    def add(self, test) -> None:
         self.tests.append(test)
 
-    def run(self, result):
+    def run(self, result: TestResult) -> None:
         for test in self.tests:
             test.run(result)
 
 
 class WasRun(TestCase):
-    def set_up(self):
-        self.log = 'set_up '
+    def set_up(self) -> None:
+        self.log: str = 'set_up '
 
-    def test_method(self):
-        self.was_run = 1
+    def test_method(self) -> None:
+        self.was_run: int = 1
         self.log += 'test_method '
 
-    def test_broken_method(self):
+    def test_broken_method(self) -> None:
         raise Exception
 
-    def tear_down(self):
+    def tear_down(self) -> None:
         self.log += 'tear_down '
