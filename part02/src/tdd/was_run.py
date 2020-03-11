15250601213cb8dc99243e119005fe38dc9525c1
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-03-11 17:24:55 +0900

    Chapter 20 is done.

--- part02/src/tdd/was_run.py
+++ part02/src/tdd/was_run.py
@@ -9,12 +9,19 @@ class TestCase:
         self.set_up()
         method = getattr(self, self.name)
         method()
+        self.tear_down()
+
+    def tear_down(self):
+        pass
 
 
 class WasRun(TestCase):
     def set_up(self):
-        self.was_run = None
-        self.was_set_up = 1
+        self.log = 'set_up '
 
     def test_method(self):
         self.was_run = 1
+        self.log += 'test_method '
+
+    def tear_down(self):
+        self.log += 'tear_down '
--- part02/tests/xunit.py
+++ part02/tests/xunit.py
@@ -2,17 +2,10 @@ from src.tdd.was_run import WasRun, TestCase
 
 
 class TestCaseTest(TestCase):
-    def set_up(self):
+    def test_template_method(self):
         self.test = WasRun("test_method")
-
-    def test_running(self):
         self.test.run()
-        assert (self.test.was_run)
-
-    def test_set_up(self):
-        self.test.run()
-        assert (self.test.was_set_up)
+        assert ('set_up test_method tear_down ' == self.test.log)
 
 
-TestCaseTest("test_running").run()
-TestCaseTest("test_set_up").run()
+TestCaseTest("test_template_method").run()
